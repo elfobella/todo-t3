b6211508eae94b7fabcedcf04c16513287e15f0b
elfobella <yunusemreucr11@gmail.com>
2023-04-08 12:16:19 +0300

    Category and task UI

--- src/pages/list/[catId].tsx
+++ src/pages/list/[catId].tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { MdArrowUpward } from "react-icons/md";
 import { api } from "y/utils/api";
 import Layout from "../Layout";
+import CategoryTodo from "y/components/CategoryTodo";
 
 const CategoryPage = () => {
   const router = useRouter();
@@ -17,44 +18,47 @@ const CategoryPage = () => {
     },
   });
 
+  const handleAdd = () => {
+    setText("");
+    if (singleCat && singleCat.title) {
+      addTodo({
+        title: text,
+        categoryId: singleCat.id,
+      });
+    } else {
+      // Kategori tanımlı değilse, hata mesajı gösterilebilir veya farklı bir değer atanabilir.
+      console.error("Category not defined.");
+    }
+  };
+
   const { data: todos } = api.todo.filteredTodo.useQuery(catId);
 
   return (
     <Layout>
-      <div className="relative">
-        <div>
-          <p className="text-lg font-semibold">{singleCat?.title}</p>
+      <div className=" flex flex-col">
+        <p className="text-lg font-semibold">{singleCat?.title}</p>
+        <div className=" crollbar mt-4 flex h-[80%] max-h-[450px] w-[420px] flex-1 flex-col overflow-y-scroll rounded-t-lg bg-gray-800 p-4 text-stone-200 scrollbar-thin scrollbar-track-gray-800 scrollbar-thumb-gray-600 dark:bg-gray-700  dark:scrollbar-track-gray-700">
+          <div>
+            {todos?.map((todo) => (
+              <CategoryTodo key={todo.id} todo={todo} />
+            ))}
+          </div>
         </div>
-        <div className="my-4">
-          {todos?.map((todo) => (
-            <div key={todo.id}>{todo.title}</div>
-          ))}
-        </div>
-        <div className="absolute bottom-0 flex items-center space-x-2">
+        <div className="flex items-center space-x-2 rounded-b-lg bg-gray-800 p-2 dark:bg-gray-700 ">
           <input
+            onKeyDown={(e) => (e.key === "Enter" ? handleAdd() : "")}
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
             placeholder="Add"
-            className="flex-1 rounded-lg bg-gray-800 p-2 text-stone-200 outline-none dark:bg-stone-500/50"
+            className="flex-1 rounded-lg  bg-gray-700 p-2 text-stone-200 outline-none dark:bg-stone-500/50"
           />
           <button
             disabled={isLoading || !text}
-            onClick={() => {
-              setText("");
-              if (singleCat && singleCat.title) {
-                addTodo({
-                  title: text,
-                  categoryId: singleCat.id,
-                });
-              } else {
-                // Kategori tanımlı değilse, hata mesajı gösterilebilir veya farklı bir değer atanabilir.
-                console.error("Category not defined.");
-              }
-            }}
+            onClick={handleAdd}
             className={`rounded-full ${
               isLoading ? "dark:bg-stone-500/50" : ""
-            } cursor-pointer bg-gray-800 p-[10px] text-stone-200 transition duration-200 hover:bg-blue-500 dark:bg-stone-500 `}
+            } cursor-pointer bg-gray-700 p-[10px] text-stone-200 transition duration-200 hover:bg-blue-500 dark:bg-stone-500 `}
           >
             <MdArrowUpward />
           </button>
--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,10 @@ import { AppRouter } from "./server/api/root";
 type RouterOutputs = inferRouterOutputs<AppRouter>;
 type allTodoOutput = RouterOutputs["todo"]["getAll"];
 type allCategoryOutput = RouterOutputs["category"]["getAll"];
+type allCategoryTodos = RouterOutputs["todo"]["filteredTodo"];
 
 export type Todo = allTodoOutput[number];
+
+export type CategoryTodo = allCategoryTodos[number];
 
 export type Cat = allCategoryOutput[number];
